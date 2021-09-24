a3d8bfc4dbf158532d337982c018a050310cf3f6
jamiebrowning <jamie.browning@collinsongroup.com>
2021-09-25 00:31:10 +0100

    Rename Run class and controller

--- src/main/java/com/example/price/RuleController.java
+++ src/main/java/com/example/price/RuleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PriceController {
+public class RuleController {
 
     @Autowired
     PriceService priceService;
