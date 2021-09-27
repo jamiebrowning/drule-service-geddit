b3f160cc62370985e732885f2045a0007364320d
jamiebrowning <jamie.browning@collinsongroup.com>
2021-09-27 17:49:26 +0100

    Separate out KB by domain

--- src/main/java/com/example/rule/RuleHandler.java
+++ src/main/java/com/example/rule/RuleHandler.java
@@ -7,11 +7,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class RuleHandler {
 
+    private static final String K_SESSION_NAME = "priceRuleKS";
+
     @Autowired
     KieContainer kContainer;
 
     <T> T fireAllRules(T context) {
-        kContainer.newStatelessKieSession().execute(context);
+        kContainer.newStatelessKieSession(K_SESSION_NAME).execute(context);
         return context;
     }
 }
